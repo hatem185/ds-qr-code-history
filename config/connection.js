7bd28b3ebc51fd9e57ae6081942e112800bf20be
hatem <hatememhemed.2000@gmail.com>
2023-12-11 19:15:44 +0200

    Change database name

--- config/connection.js
+++ config/connection.js
@@ -26,7 +26,7 @@ async function getConnectionInfo() {
   }
 
   // To override the database name, set the DATABASE_NAME environment variable in the .env file
-  const DATABASE_NAME = process.env.DATABASE_NAME || "ds-system-database";
+  const DATABASE_NAME = process.env.DATABASE_NAME || "dssystem-database";
 
   return {
     DATABASE_URL: process.env.DATABASE_URL,
