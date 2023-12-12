6eed76a842d64599a4e581b236151fbbf331ebce
hatem <hatememhemed.2000@gmail.com>
2023-12-12 12:36:46 +0200

    Fix connection string issue

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ import { config } from "dotenv";
 import express from "express";
 import cors from "cors";
 import { connect } from "./config/database.js"; // Import
-// import { getConnectionInfo } from "./config/connection";
+import { getConnectionInfo } from "./config/connection";
 import qrCodeHistoryRoutes from "./app/routes/qrCodeHistoryRoutes.js";
 config();
 const app = express();
 const port = 3000; // Choose the port number you prefer
 // const db_connection_string = process.env.MONGODB_URI;
-const db_connection_string = process.env.DATABASE_URL;
-
+const ENVITONMENT = process.env.ENVIRONMENT;
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -20,7 +19,14 @@ app.get("/api", async (_, res) => {
 
 // Start the server
 app.listen(port, async () => {
-  // var connectionInfo = await getConnectionInfo();
+  let db_connection_string = "";
+
+  if (ENVITONMENT === "development") {
+    db_connection_string = process.env.DATABASE_URL;
+  } else {
+    db_connection_string = (await getConnectionInfo()).DATABASE_URL;
+  }
   connect(db_connection_string);
+
   console.log(`Server running on port ${port}`);
 });
